1b6c50ebbf7455974186d74c39d6998b63abdd59
AyaIsmail2003 <120684040+Ayaismail2022@users.noreply.github.com>
2025-03-05 16:12:38 +0200

    Add files via upload

---
+++ Task3_Day5_AyaIsmail.py
@@ -0,0 +1,29 @@
+print(265)
+print(-2)
+print(0.985)
+print(-0.985)
+print(9+5j)
+
+num = 1+2j
+print("Imagine number is: {} ".format(num.imag))
+print("Real number is: {} ".format(num.real))
+
+num1 = 10
+print(f"{num1:.10f}")
+
+num2 = 159.650
+integer =int(num2)
+print(integer)
+print(type(integer))
+
+# 100 ? 115 = -15
+# 50 ? 30 = 1500
+# 21 ? 4 = 1
+# 110 ? 11 = 10
+# 97 ? 20 = 4
+
+print(100-115)
+print(50*30)
+print(21 % 4)
+print(110/11)
+print(97//20)
